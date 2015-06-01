9e84389d690f19b03ba7e7a3fc9bd8c67a4a3c5f
Christian Betz <christian.betz@performance-media.de>
2015-06-01 12:36:37 +0200

    cleaned up logging in tesla, so add it here

--- simple-example/project.clj
+++ simple-example/project.clj
@@ -1,10 +1,13 @@
 (defproject de.otto/tesla-simple-example "0.1.0"
             :description "a simple example of an application build with tesla-microservice."
             :url "https://github.com/otto-de/tesla-examples"
-            :license { :name "Apache License 2.0" 
+            :license { :name "Apache License 2.0"
                        :url "http://www.apache.org/license/LICENSE-2.0.html"}
             :scm { :name "git"
  	           :url "https://github.com/otto-de/tesla-microservice"}
             :dependencies [[org.clojure/clojure "1.6.0"]
-                           [de.otto/tesla-microservice "0.1.12"]]
+                           [de.otto/tesla-microservice "0.1.12"]
+                           [org.slf4j/slf4j-api "1.7.12"]
+                           [ch.qos.logback/logback-core "1.1.3"]
+                           [ch.qos.logback/logback-classic "1.1.3"]]
             :main ^:skip-aot de.otto.tesla.example.example-system)
